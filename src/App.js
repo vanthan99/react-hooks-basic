0d00a28a3613190c7d3f55f5d1693ddcb03eb09c
vanthan99 <vanthan.ad.it@gmail.com>
2020-08-23 15:53:50 +0700

    react hook 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
+  const [todoList, setTodoList] = useState(
+    [
+      { id: 1, title: 'I love Easy Frontend! 😍 ' },
+      { id: 2, title: 'We love Easy Frontend! 🥰 ' },
+      { id: 3, title: 'They love Easy Frontend! 🚀 ' },
+    ]
+  );
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex(item => item.id === todo.id);
+    if (index < 0) return;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  function handleTodoFormSubmit(values) {
+    const value = {
+      id: todoList.length + 1,
+      ...values
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(value);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="app">
       <h1>welcome to react js hooks</h1>
-      <ColorBox />
+      <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList
+        todos={todoList}
+        onTodoClick={handleTodoClick}
+      />
     </div>
   );
 }
