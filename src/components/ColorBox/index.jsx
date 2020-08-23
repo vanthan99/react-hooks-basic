56c1968e42637bc631f07445de5d26da78f37d61
vanthan99 <vanthan.ad.it@gmail.com>
2020-08-23 10:20:05 +0700

    react hook 3

---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './ColorBox.scss'
+
+function getRandomColor() {
+    const COLOR_LIST = ['deeppink', 'green', 'yellow', 'black', 'blue'];
+    const randomIndex = Math.trunc(Math.random()*5);
+    return COLOR_LIST[randomIndex];
+}
+
+function ColorBox() {
+    
+    const [color,setColor] = useState(() => {
+        const initColor = localStorage.getItem('color-box') || 'deeppink';
+        return initColor;
+    });
+    
+    function handleBoxClick() {
+        // get random color --> set color
+        const newColor = getRandomColor();
+        setColor(newColor);
+        localStorage.setItem('color-box',newColor);
+    }
+    
+    return (
+        <div
+            className='color-box'
+            style={{backgroundColor:color}}
+            onClick={handleBoxClick}
+        >
+            COLOR BOX
+        </div>
+    );
+}
+
+export default ColorBox;
